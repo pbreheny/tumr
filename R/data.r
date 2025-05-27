e584bd5bb031f5b75ecbd6bad4f6414b5cb100d2
Patrick Breheny <patrick-breheny@uiowa.edu>
2025-05-27 13:56:58 -0500

    minor linting

--- R/data.r
+++ R/data.r
@@ -1,27 +1,40 @@
 #' Tumor growth data for a melanoma cancer model (A375)
 #'
-#' The A375 cell line (metastatic melanoma) was used to create CDX (Cell Line Derived Xenograft) mice.
+#' The A375 cell line (metastatic melanoma) was used to create CDX (Cell Line
+#' Derived Xenograft) mice.
 #'
 #' @format A data frame with 600 rows and 4 variables:
 #' \describe{
 #'   \item{ID}{Mouse identifier to indicate repeated measurements on the same mouse. (factor)}
 #'   \item{Day}{Day of measurment (integer)}
 #'   \item{Volume}{Tumor volume (double)}
-#'   \item{Treatment}{Treatment group (factor). A = control, B = Vemurafenib (BRAF inhibitor), C = Buthionine sulfoximine (BSO), D = Vemurafenib + BSO.}
+#'   \item{Treatment}{Treatment group (factor).
+#'     A = control,
+#'     B = Vemurafenib (BRAF inhibitor),
+#'     C = Buthionine sulfoximine (BSO),
+#'     D = Vemurafenib + BSO.
+#'   }
 #' }
 #' @source University of Iowa, Holden Cancer Center, experiment SLB19-022.
 "melanoma1"
 
 #' Tumor growth data for a melanoma cancer model (451LuBR)
 #'
-#' The 451LuBR cell line (metastatic melanoma) was used to create CDX (Cell Line Derived Xenograft) mice.
+#' The 451LuBR cell line (metastatic melanoma) was used to create CDX (Cell Line
+#' Derived Xenograft) mice.
 #'
 #' @format A data frame with 568 rows and 4 variables:
 #' \describe{
 #'   \item{ID}{Mouse identifier to indicate repeated measurements on the same mouse. (factor)}
 #'   \item{Day}{Day of measurment (integer)}
 #'   \item{Volume}{Tumor volume (double)}
-#'   \item{Treatment}{Treatment group (factor). A = control, B = PB-212 (lead), C = PLX4032 (BRAF inhibitor), D = PLX4032 + PBA (4-Phenylbutyric acid), E = PB-212 + PLX4032 + PBA.}
+#'   \item{Treatment}{Treatment group (factor).
+#'     A = control,
+#'     B = PB-212 (lead),
+#'     C = PLX4032 (BRAF inhibitor),
+#'     D = PLX4032 + PBA (4-Phenylbutyric acid),
+#'     E = PB-212 + PLX4032 + PBA.
+#'   }
 #' }
 #' @source University of Iowa, Holden Cancer Center, experiment GJZ16-091.
 "melanoma2"
@@ -29,15 +42,19 @@
 #' Tumor growth data for a breast cancer model
 #'
 #' In this strain of rat, tumors begin to appear about 6 weeks after carcinogen
-#' and are followed for another 6 weeks. The goal of the study is to estimate the
-#' effects of nicotinamide riboside (a dietary supplement) on growth of mammary
-#' gland tumors.
+#' and are followed for another 6 weeks. The goal of the study is to estimate
+#' the effects of nicotinamide riboside (a dietary supplement) on growth of
+#' mammary gland tumors.
 #'
 #' @format A data frame with 336 rows and 6 variables:
 #' \describe{
 #'   \item{ID}{Mouse identifier to indicate repeated measurements on the same mouse. (factor)}
-#'   \item{Cohort}{Indicator of which cohort the mouse belonged to, in order to account for potential batch effects. (factor)}
-#'   \item{Treatment}{Treatment group (factor). VEH = control (vehicle), NR = nicotinamide riboside}
+#'   \item{Cohort}{Indicator of which cohort the mouse belonged to, in order to
+#'     account for potential batch effects. (factor)}
+#'   \item{Treatment}{Treatment group (factor).
+#'     VEH = control (vehicle),
+#'     NR = nicotinamide riboside
+#'   }
 #'   \item{Week}{Week of measurment (integer)}
 #'   \item{Volume}{Tumor volume, in cubic mm (double)}
 #'   \item{Number}{Number of tumors (integer)}
@@ -58,7 +75,11 @@
 #'   \item{ID}{Mouse identifier to indicate repeated measurements on the same mouse. (factor)}
 #'   \item{Age}{Age of the mouse, in weeks (integer)}
 #'   \item{BLI}{Tumor volume, as measured by bioluminescent imaging, in photons (double)}
-#'   \item{Genotype}{p53 genotype (factor). WT = wild type, HET = single knockout, DOKO = double knockout.}
+#'   \item{Genotype}{p53 genotype (factor).
+#'     WT = wild type,
+#'     HET = single knockout,
+#'     DOKO = double knockout.
+#'   }
 #' }
 #' @source https://doi.org/10.1371/journal.pone.0232807
 "prostate"
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Tools for analyzing tumor growth data
 
 <!-- badges: start -->
-[![GitHub version](https://img.shields.io/static/v1?label=GitHub&message=0.5.0&color=blue&logo=github)](https://github.com/pbreheny/tumr)
+[![GitHub version](https://img.shields.io/static/v1?label=GitHub&message=0.5.1&color=blue&logo=github)](https://github.com/pbreheny/tumr)
 [![R-CMD-check](https://github.com/pbreheny/tumr/workflows/R-CMD-check/badge.svg)](https://github.com/pbreheny/tumr/actions)
 <!-- badges: end -->

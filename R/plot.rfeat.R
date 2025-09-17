13a7c483d5bceb7f4179d06679cf28352ed8bf62
Plowman <splwman@uiowa.edu>
2025-09-17 08:35:37 -0500

    moving plot.rfeat into a different script

---
+++ R/plot.rfeat.R
@@ -0,0 +1,17 @@
+
+
+plot.rfeat <- function (x) {
+
+  ggplot2::ggplot(x$betas, ggplot2::aes(x = Group, y = Beta, color = Group)) +
+    ggplot2::geom_jitter(width = 0.2, size = 2) +
+    ggplot2::stat_summary(fun = mean, geom = "point", shape = 18, size = 4, color = "black") +
+    ggplot2::labs(title = "Individual Slopes (Betas) by Group",
+                  y = paste("Slope of", x$relevant_information["Measure"], "vs", x$relevant_information["Time"]),
+                  x = x$relevant_information["Group"]) +
+    ggplot2::theme_minimal()
+
+}
+
+example <- rfeat(breast, "ID", "Week", "Volume", "Treatment", transformation = log1p, comparison = "t.test")
+
+plot.rfeat(example)
--- R/plots.R
+++ R/plots.R
@@ -1,39 +1,4 @@
 
-#Breast Data
-
-#NA values ignored
-
-breast_summary <- breast |>
-  dplyr::group_by(Treatment, Week) |>
-  dplyr::summarise(Volume = median(Volume)) |>
-  dplyr::ungroup()
-
-ggplot2::ggplot() +
-  ggplot2::geom_line(data = breast, ggplot2::aes(x = Week, y = Volume, group = ID), alpha = 0.3)  +
-  ggplot2::geom_line(data = breast_summary, ggplot2::aes(x = Week, y = Volume, color = Treatment), linewidth = 1.2)
-
-#log transformed data
-
-breast_2 <- breast |>
-  dplyr::mutate(log_y = log1p(Volume))
-
-breast_summary <- breast |>
-  dplyr::mutate(log_y = log1p(Volume)) |>
-  dplyr::group_by(Treatment, Week) |>
-  dplyr::summarise(log_y = mean(log_y)) |>
-  dplyr::ungroup()
-
-ggplot2::ggplot() +
-  ggplot2::geom_line(data = breast_2, ggplot2::aes(x = Week, y = log_y, group = ID), alpha = 0.3)  +
-  ggplot2::geom_line(data = breast_summary, ggplot2::aes(x = Week, y = log_y, color = Treatment), linewidth = 1.2)
-
-
-
-breast_exp <- breast |>
-  mutate(log_y = log1p(Volume))
-
-
-
 
 #default summary measure is the median
 #drop_last in summarise all the time or option?
@@ -64,22 +29,6 @@ plots(melanoma2, Treatment, Day, Volume, ID)
 plots(prostate, Genotype, Age, BLI, ID)
 
 
-#plot.rfeat
 
-plot.rfeat <- function (x) {
-
-  ggplot2::ggplot(x$betas, ggplot2::aes(x = Group, y = Beta, color = Group)) +
-  ggplot2::geom_jitter(width = 0.2, size = 2) +
-  ggplot2::stat_summary(fun = mean, geom = "point", shape = 18, size = 4, color = "black") +
-  ggplot2::labs(title = "Individual Slopes (Betas) by Group",
-                y = paste("Slope of", x$relevant_information["Measure"], "vs", x$relevant_information["Time"]),
-                x = x$relevant_information["Group"]) +
-  ggplot2::theme_minimal()
-
-}
-
-example <- rfeat(breast, "ID", "Week", "Volume", "Treatment", transformation = log1p, comparison = "t.test")
-
-plot.rfeat(example)
 
 

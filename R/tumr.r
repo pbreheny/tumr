932683b34aa985a8a854ab7f9fc5af52f351a446
Patrick Breheny <patrick-breheny@uiowa.edu>
2025-09-30 11:34:28 -0500

    importing .data from rlang

--- R/tumr.r
+++ R/tumr.r
@@ -1,5 +1,6 @@
 #' tumr: Analysis and Power Calculations for Tumor Growth Rates
 #' Analysis and power calculations for tumor growth rates.
 #' @import stats
+#' @importFrom rlang .data `%||%`
 "_PACKAGE"
 

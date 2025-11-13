3724e671b45695a3ac30f9dc72a4fc15a495bcbc
Sydney Plowman <sydneyplowman@Sydneys-MacBook-Air-200.local>
2025-11-12 20:10:57 -0600

    created plot.lmm function

---
+++ R/plot.lmm.R
@@ -0,0 +1,35 @@
+#' Creates Plots of an lmm object
+#'
+#' @param x lmm object
+#' @param ... further arguments passed to or from other methods
+#'
+#' @return 2 plots
+#'
+#' @examples
+#' mel1 <- mel1 <- tumr(melanoma1, ID, Day, Volume, Treatment)
+#' mel1_lmm <- lmm(mel1)
+#' plot(mel1_lmm)
+#'
+#' @export
+
+plot.lmm <- function(x, ...){
+  model <- x$model_sum
+
+  pred <- ggeffects::ggpredict(model,
+                               terms = c(x$relevant_info$Time,
+                                         x$relevant_info$Group))
+  pred_plot <- plot(pred)
+
+  emm_sum <- summary(x)
+
+  emm_plot <- plot(emm_sum$`slope of treatment over time`)
+
+  return(list(
+    pred_plot,
+    emm_plot
+  ))
+
+}
+
+
+

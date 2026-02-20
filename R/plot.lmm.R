3280b378c3f3e39fc0f2bfb7423f7e26e0564419
Tian Zhao <102346643+Gapomochi@users.noreply.github.com>
2026-02-19 18:06:46 -0600

    update print option for plot.lmm

--- R/plot.lmm.R
+++ R/plot.lmm.R
@@ -1,34 +1,33 @@
 #' Creates Plots of an lmm object
 #'
 #' @param x lmm object
+#' @param type Character string specifying which plot to produce. One of
+#'   \code{"predict"} or \code{"slope"}.
 #' @param ... further arguments passed to or from other methods
-#'
-#' @return 2 plots
+#' @return A list of ggplot objects.
 #'
 #' @examples
 #' mel1 <- mel1 <- tumr(melanoma1, ID, Day, Volume, Treatment)
 #' mel1_lmm <- lmm(mel1)
-#' plot(mel1_lmm)
-#'
+#' plot(mel1_lmm, "predict")
+#' plot(mel1_lmm, "slope")
 #' @export
 
-plot.lmm <- function(x, ...){
+plot.lmm <- function(x, type = c("predict", "slope"), ...){
   model <- x$model_sum
+  if (type == "predict") {
+    pred <- ggeffects::ggpredict(model,
+                                 terms = c(x$relevant_info$Time,
+                                           x$relevant_info$Group))
+    pred_plot <- plot(pred)
+    pred_plot
+  }
 
-  pred <- ggeffects::ggpredict(model,
-                               terms = c(x$relevant_info$Time,
-                                         x$relevant_info$Group))
-  pred_plot <- plot(pred)
-
-  emm_sum <- summary(x)
-
-  emm_plot <- plot(emm_sum$`slope of treatment over time`)
-
-  return(list(
-    predicted_measure = pred_plot,
-    mean_betas = emm_plot
-  ))
-
+  if (type == "slope") {
+    emm_sum <- summary(x)
+    emm_plot <- plot(emm_sum$`slope of treatment over time`)
+    emm_plot
+  }
 }
 
 

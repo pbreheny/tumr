858ae434d81610ff419011b72664be200a9666f1
Tian Zhao <102346643+Gapomochi@users.noreply.github.com>
2026-09-01 20:48:23 -0500

    added warning for tumr() to check time scale

--- R/tumr.R
+++ R/tumr.R
@@ -14,13 +14,28 @@
 #'
 #' @export
 
-
 tumr <- function(data, id, time, measure, group){
   id <- deparse(substitute(id))
   time <- deparse(substitute(time))
   measure <- deparse(substitute(measure))
   group <- deparse(substitute(group))
-
+  # check time scale
+  time_values <- data[[time]]
+  if (is.numeric(time_values) &&
+      any(!is.na(time_values)) &&
+      diff(range(time_values, na.rm = TRUE)) > 50) {
+    warning(
+      paste0(
+        "\n----------------------------------------------------------------------\n",
+        "The range of the time variable is greater than 50, which may ",
+        "cause convergence issues when fitting lmm().\n",
+        "Consider rescaling time to a larger unit ",
+        "(for example, from days to weeks or months).\n",
+        "----------------------------------------------------------------------"
+      ),
+      call. = FALSE
+    )
+  }
   meta_data <- list(
     id = id,
     time = time,
@@ -31,5 +46,3 @@ tumr <- function(data, id, time, measure, group){
   class(meta_data) <- "tumr"
   return(meta_data)
 }
-
-
